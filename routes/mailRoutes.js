fed40f2bcbcd08e1af6f684c3ff9db6ed2fbc46f
Shivam <sg641818@gmail.com>
2025-07-19 13:55:26 +0530

    removing middleware to test sending mail

--- routes/mailRoutes.js
+++ routes/mailRoutes.js
@@ -3,6 +3,6 @@ import { sendMail } from '../controllers/mailController.js';
 import { protect } from '../middleware/auth.js';
 const router = express.Router();
 
-router.post('/send', protect, sendMail);
+router.post('/send', sendMail);
 
 export default router;
